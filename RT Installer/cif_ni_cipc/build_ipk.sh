c0c7b30fc2e026f31275c8384187e841b48ddb1a
DOME_WIN1\toddw <todd.walter@gmail.com>
2025-12-31 08:26:29 -0600

    Updated RT installer.

--- RT Installer/cif_ni_cipc/build_ipk.sh
+++ RT Installer/cif_ni_cipc/build_ipk.sh
@@ -9,7 +9,7 @@ tar --numeric-owner --group=0 --owner=0 -czf ../control.tar.gz ./*
 popd
 
 #pushd /home/admin/ipk_maker/adp-vsim/
-tar --numeric-owner --group=0 --owner=0 -cf ../cif_ni_cipc.0.1.0.ipk ./debian-binary ./data.tar.gz ./control.tar.gz
+tar --numeric-owner --group=0 --owner=0 -cf ../cif_ni_cipc.0.1.1.ipk ./debian-binary ./data.tar.gz ./control.tar.gz
 rm ./data.tar.gz
 rm ./control.tar.gz
 #popd
